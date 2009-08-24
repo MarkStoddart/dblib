d358a013af798be9892a2eb49fb7d189161264eb
jamiehurst <none@none>
2009-08-24 16:25:50 +0100

    Changed a couple of interface problems, added the connectDB(), setupDB() and closeDB() functions.

--- mysqlDB.class.php
+++ mysqlDB.class.php
@@ -14,28 +14,21 @@ require 'iDB.interface.php';
  */
 public class mysqlDB implements iDB {
 	
-	private var $DBobject;
-	private var $DBhost;
-	private var $DBuser;
-	private var $DBpass;
-	private var $DBname;
+	private var $db;
+	private var $host;
+	private var $Duser;
+	private var $pass;
+	private var $name;
 	private var $queryCount;
 	
 	/**
 	 * Constructor
-	 * Use the provided DB variable if set, otherwise connect
-	 * using the connectDB() method
+	 * Use the provided DB variable if set, otherwise don't connect just yet
 	 * 
 	 * @param	mixed	$p_db	[Optional] MySQL connection link
 	 */
-	public function __construct($p_db = null) {
-		if(is_null($p_db)) {
-			// Connect to the database, using the connect method
-			$this->DBobject = $this->connectDB();
-		} else {
-			// Leave it otherwise, even if the variable isn't a correct link
-			$this->DBobject = $p_db;
-		}
+	public function __construct($p_db = false) {
+		$this->db = $p_db;
 	}
 	
 	/**
@@ -43,8 +36,64 @@ public class mysqlDB implements iDB {
 	 * Close the database
 	 */
 	public function __destruct() {
-		if($this->DBobject)
-			mysql_close($this->DBobject);
+		$this->closeDB();
+	}
+	
+	/**
+	 * Set the parameters used to connect to the database
+	 * 
+	 * @param	string	$p_host	[Optional] Database host
+	 * @param	string	$p_user	[Optional] Database username
+	 * @param	string	$p_pass	[Optional] Database password
+	 * @param	string	$p_db	[Optional] Database to use
+	 */
+	public function setupDB($p_host = null, $p_user = null, $p_pass = null, $p_db = null) {
+		if(!is_null($p_host))
+			$this->host = $p_host;
+		if(!is_null($p_user))
+			$this->user = $p_user;
+		if(!is_null($p_pass))
+			$this->pass = $p_pass;
+		if(!is_null($p_db))
+			$this->name = $p_db;
+	}
+	
+	/**
+	 * Connect to the database using the parameters given, or those already present
+	 * 
+	 * @param	string	$p_host	[Optional] Database host
+	 * @param	string	$p_user	[Optional] Database username
+	 * @param	string	$p_pass	[Optional] Database password
+	 * @param	string	$p_db	[Optional] Database to use
+	 * @return	boolean	Success or not
+	 */
+	public function connectDB($p_host = null, $p_user = null, $p_pass = null, $p_db = null) {
+		// Call setupDB() to handle the parameters
+		$this->setupDB($p_host, $p_user, $p_pass, $p_db);
+		
+		// Attempt a connection
+		$this->db = mysql_connect($this->host, $this->user, $this->pass);
+		if(!$this->db) {
+			$this->errorDB('connect');
+			return false;
+		}
+		
+		// Select the database to use
+		$select = mysql_select_db($this->name, $this->db);
+		if(!$select) {
+			$this->errorDB('connect_db');
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Close the active database connection if one exists
+	 */
+	public function closeDB() {
+		if($this->db)
+			mysql_close($this->db);
 	}
 	
 	// TODO Do the rest of it
