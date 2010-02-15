9bbb7b0687b060cd537801cd36bb1d75a07ffb00
Zenithus <zenithus@zenco.net>
2010-02-15 08:30:44 +0000

    Continuing tests...

--- tests/mysqlDBTest.php
+++ tests/mysqlDBTest.php
@@ -13,7 +13,9 @@ class mysqlDBTest extends PHPUnit_Framework_TestCase {
 		$init = mysql_connect('localhost', 'root', 'ga6-bxd');
 		mysql_select_db('test', $init);
 		mysql_query("
-			DROP TABLE IF EXISTS testing;
+			DROP TABLE testing
+		", $init);
+		mysql_query("
 			CREATE TABLE testing (
 			  `id` int(11) NOT NULL AUTO_INCREMENT,
 			  `subject` varchar(255) DEFAULT NULL,
@@ -45,9 +47,44 @@ class mysqlDBTest extends PHPUnit_Framework_TestCase {
 	public function testInsertRow() {
 		$this->db->connectDB('localhost', 'root', 'ga6-bxd', 'test');
 		$insert = $this->db->insertRow('testing', array(
-				'subject'	=>	'TEST',
-				'content'	=>	'Test One'
-			));
+			'subject'	=>	'TEST1',
+			'content'	=>	'Test One'
+		));
 		$this->assertTrue($insert);
+	}
+	
+	public function testGetField() {
+		$this->db->connectDB('localhost', 'root', 'ga6-bxd', 'test');
+		$this->db->insertRow('testing', array(
+			'subject'	=>	'TEST2',
+			'content'	=>	'Test Two'
+		));
+		$field = $this->db->getField('content', 'testing', 'WHERE `subject` = ?', 'TEST2');
+		$this->assertEquals($field, 'Test Two');
+	}
+	
+	public function testGetRow() {
+		$this->db->connectDB('localhost', 'root', 'ga6-bxd', 'test');
+		$this->db->insertRow('testing', array(
+			'subject'	=>	'TEST3',
+			'content'	=>	'Test Three'
+		));
+		$row = $this->db->getRow('testing', 'WHERE `subject` = ?', 'TEST3');
+		$this->assertEquals($row['content'], 'Test Three');
+	}
+
+	public function testGetRows() {
+		$this->db->connectDB('localhost', 'root', 'ga6-bxd', 'test');
+		$this->db->insertRow('testing', array(
+			'subject'	=>	'TEST_MULTIPLE',
+			'content'	=>	'Test Four'
+		));
+		$this->db->insertRow('testing', array(
+			'subject'	=>	'TEST_MULTIPLE',
+			'content'	=>	'Test Five'
+		));
+		$rows = $this->db->getRows('testing', 'WHERE `subject` = ?', 'TEST_MULTIPLE');
+		$this->assertEquals($rows[0]['content'], 'Test Four');
+		$this->assertEquals($rows[1]['content'], 'Test Five');
 	}
 }
